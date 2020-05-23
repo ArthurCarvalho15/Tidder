4e204dd2ca6e4527b16226fd4c59525477a48e05
VictorLuizFerreira <61093338+VictorLuizFerreira@users.noreply.github.com>
2020-05-23 05:17:39 -0300

    COMPLETÃO

---
+++ FULL/app/views/home.php
@@ -0,0 +1,121 @@
+<div id="main">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-5 col-xs-12">
+				<div id="meu_carrossel" class="carousel slide" data-ride="carousel">
+
+					<!-- PONTOS REPRESENTATIVOS DE IMAGENS -->
+					<ol class="carousel-indicators">
+    					<li data-target="#meu_carrossel" data-slide-to="0" class="active"></li>
+    					<li data-target="#meu_carrossel" data-slide-to="1"></li>
+						<li data-target="#meu_carrossel" data-slide-to="2"></li>
+						<li data-target="#meu_carrossel" data-slide-to="3"></li>
+  					</ol>
+
+					<!-- IMAGENS -->
+					<div class="carousel-inner" role="listbox">
+						<div class="item active">
+							<img src="/betabase_alfa/app/img/slide1.png" class="img-thumbnail center" alt="...">
+							<div class="carousel-caption">
+								<strong>Redes sociais</strong> e negócios de todos os tipos, com engajamento entre empresa e público.
+							</div>
+						</div>
+					
+						<div class="item">
+							<img src="/betabase_alfa/app/img/slide2.png" class="img-thumbnail"  alt="...">
+							<div class="carousel-caption">
+								Grandes plataformas conseguem oferecer uma seleção cada vez maior de produtos, tendo pequenos lojistas integrados. 
+							</div>
+						</div>
+
+						<div class="item">
+							<img src="/betabase_alfa/app/img/slide3.png" class="img-thumbnail"  alt="...">
+							<div class="carousel-caption">
+								Permitir executar pagamentos e transferências sem sair do site da loja, ou fornecedor do serviço.
+							</div>
+						</div>
+
+						<div class="item">
+							<img src="/betabase_alfa/app/img/slide4.png" class="img-thumbnail"  alt="...">
+							<div class="carousel-caption">
+								Permitem visualizar dados sobre topografia e geografia e ajudam a determinar previsões de clima.
+							</div>
+						</div>
+
+
+					</div>
+
+					<!-- CONTROLES -->
+					<a class="left carousel-control" href="#meu_carrossel" role="button" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+						<span class="sr-only">Anterior</span>
+					</a>
+					<a class="right carousel-control" href="#meu_carrossel" role="button" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+						<span class="sr-only">Próximo</span>
+					</a>
+				</div>
+			</div>
+
+			<div class="col-md-7 col-xs-12">
+				<h1>O que significa API e como ela funciona!</h1>
+				<p>A API é um conjunto de definições e protocolos usado no desenvolvimento e na integração de software
+					de aplicações. API é um acrônimo em inglês que significa interface de programação de aplicações.
+					Com as APIs, sua solução ou serviço pode se comunicar com outros produtos e serviços sem precisar
+					saber como eles foram implementados. Isso simplifica o desenvolvimento de aplicações,
+					gerando economia de tempo e dinheiro. Ao desenvolver novas ferramentas e soluções 
+					(ou ao gerenciar aquelas já existentes), as APIs oferecem a flexibilidade necessária para simplificar
+					o design, a administração e o uso, além de fornecer oportunidades de inovação.
+
+			</p>
+				<p><a class="btn btn-primary btn-lg" href="#" role="button">Leia mais</a></p>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="middle">
+	<div class="container">
+		<div class="row"> 
+
+			<div class="col-sm-6 col-md-4"> 
+				<div class="thumbnail"> 
+					<img src="/betabase_alfa/app/img/watson.png" class="img-thumbnail"> 
+
+					<div class="caption"> 
+						<h3>IBM WATSON</h3> 
+						<p>O Watson já cobre cerca de uma dúzia de APIs diferentes que ajudarão você a entender imagens, som e texto. 
+							Algumas dessas APIs são: a API de reconhecimento visual, a Tone Analyzer API e a API Explorer do Watson. 
+							Você insere seu conjunto de treinamento e, em seguida, a API aprende o suficiente para começar a responder perguntas.</p> 
+						<p><a href="https://www.ibm.com/watson" class="btn btn-primary" role="button">Saiba Mais</a> 
+					</div> 
+				</div> 
+			</div> 
+
+			<div class="col-sm-6 col-md-4"> 
+				<div class="thumbnail"> 
+					<img src="/betabase_alfa/app/img/webdam.jpg" class="img-thumbnail"> 
+					<div class="caption"> 
+						<h3>Webdam Rest</h3> 
+						<p>Oferecendo armazenamento em nuvem seguro com um fluxo de trabalho moderado e organizado. 
+						   As fotos e os trabalhos de arte chegam dos criadores como arquivos, mas eles se tornam "ativos" 
+						   dentro do sistema à medida que passam pela cadeia de aprovação dos anúncios, sites e folhetos que definem uma marca.
+						   A API Webdam permite que você aproveite seu próprio código interno e confie no Webdam como o sistema de arquivos que armazena e organiza suas imagens.</p> 
+						<p><a href="https://www.damsuccess.com/hc/en-us/articles/202134055-REST-API" class="btn btn-primary" role="button">Saiba Mais</a> 
+					</div> 
+				</div> 
+			</div> 
+			
+			<div class="col-sm-6 col-md-4"> 
+				<div class="thumbnail"> 
+					<img src="/betabase_alfa/app/img/amazon.JPG" class="img-thumbnail"> 
+					<div class="caption"> 
+						<h3>BlazingText</h3> 
+						<p>O algoritmo BlazingText do Amazon SageMaker ajuda a compreender as palavras, marcando o texto com dados que descrevem como a palavra foi usada anteriormente. Você insere texto bruto e recupera um vetor de significado semântico e classificação de entidade que pode ser usado para análise de sentimento. Se você quiser mais poder computacional, os algoritmos podem rodar mais rápido em máquinas com múltiplos núcleos e GPUs.
+						</p> <p><a href="https://docs.aws.amazon.com/sagemaker/latest/dg/blazingtext.html" class="btn btn-primary" role="button">Saiba Mais</a>
+					</div> 
+				</div> 
+			</div>  
+
+		</div>
+	</div>
+</div>
---
+++ FULL/app/views/navegar.php
@@ -0,0 +1,112 @@
+<div id="main" class="center">
+	<span style="color:#9bbd46">- NAVEGAR -</span>
+	<h2>Mais APIs de DESENVOLVIMENTO</h2>
+</div>
+
+
+<div id="navegar">
+	<div class="container">
+		<div class="row"> 
+
+			<div class="col-sm-6 col-md-4"> 
+				<div class="thumbnail"> 
+					<img src="/betabase_alfa/app/img/watson.png" class="img-thumbnail"> 
+
+					<div class="caption"> 
+						<h3>IBM WATSON</h3> 
+						<p>O Watson já cobre cerca de uma dúzia de APIs diferentes que ajudarão você a entender imagens, som e texto. 
+							Algumas dessas APIs são: a API de reconhecimento visual, a Tone Analyzer API e a API Explorer do Watson. 
+							Você insere seu conjunto de treinamento e, em seguida, a API aprende o suficiente para começar a responder perguntas.</p> 
+						<p><a href="https://www.ibm.com/watson" class="btn btn-primary" role="button">Saiba Mais</a> 
+					</div> 
+				</div> 
+			</div> 
+
+			<div class="col-sm-6 col-md-4"> 
+				<div class="thumbnail"> 
+					<img src="/betabase_alfa/app/img/microsoft.png" class="img-thumbnail"> 
+					<div class="caption"> 
+						<h3>Immutable Azure Blob Storage</h3> 
+						<p>Gostaríamos de acreditar que nossos arquivos digitais oferecem provas de disputas legais e financeiras, mas isso só funciona se pudermos confiar nesses arquivos.Algo proporcionado pela “immutable blob storage API".</p> <p><a href="https://docs.microsoft.com/en-us/azure/storage/blobs/storage-blob-immutable-storage" class="btn btn-primary" role="button">Saiba Mais</a>
+					</div> 
+				</div> 
+			</div> 
+			
+			<div class="col-sm-6 col-md-4"> 
+				<div class="thumbnail"> 
+					<img src="/betabase_alfa/app/img/amazon.JPG" class="img-thumbnail"> 
+					<div class="caption"> 
+						<h3>BlazingText</h3> 
+						<p>O algoritmo BlazingText do Amazon SageMaker ajuda a compreender as palavras, marcando o texto com dados que descrevem como a palavra foi usada anteriormente. Você insere texto bruto e recupera um vetor de significado semântico e classificação de entidade que pode ser usado para análise de sentimento. Se você quiser mais poder computacional, os algoritmos podem rodar mais rápido em máquinas com múltiplos núcleos e GPUs.
+						</p> <p><a href="https://docs.aws.amazon.com/sagemaker/latest/dg/blazingtext.html" class="btn btn-primary" role="button">Saiba Mais</a>
+					</div> 
+				</div> 
+			</div> 
+
+			<div class="col-sm-6 col-md-4"> 
+				<div class="thumbnail"> 
+					<img src="/betabase_alfa/app/img/amatino.jpg" class="img-thumbnail"> 
+					<div class="caption"> 
+						<h3>Amatino</h3> 
+						<p>A Amatino é uma startup que oferece uma API que lida com muitas das tarefas mais importantes para você, para se concentrar nas ideias maiores e mais grandiosas que trarão consigo receitas suficientes para que você tenha algo para contar.</p> 
+						<p><a href="https://amatino.io/" class="btn btn-primary" role="button">Saiba Mais</a>
+					</div> 
+				</div> 
+			</div> 
+
+			<div class="col-sm-6 col-md-4"> 
+				<div class="thumbnail"> 
+					<img src="/betabase_alfa/app/img/twilio.jpg" class="img-thumbnail"> 
+		
+					<div class="caption"> 
+						<h3>Twilio</h3>
+						<p>O Twilio foi projetado para simplificar a interface de seus aplicativos com as antigas capacidades de voz e texto dos telefones.</p> 
+						<p><a href="https://www.twilio.com/" class="btn btn-primary" role="button">Saiba Mais</a> 
+					</div> 
+				</div> 
+			</div>
+
+			<div class="col-sm-6 col-md-4"> 
+				<div class="thumbnail"> 
+					<img src="/betabase_alfa/app/img/webdam.jpg" class="img-thumbnail"> 
+					<div class="caption"> 
+						<h3>Webdam</h3> 
+						<p>Oferecendo armazenamento em nuvem seguro com um fluxo de trabalho moderado e organizado. 
+						   As fotos e os trabalhos de arte chegam dos criadores como arquivos, mas eles se tornam "ativos" 
+						   dentro do sistema à medida que passam pela cadeia de aprovação dos anúncios, sites e folhetos que definem uma marca.
+						   A API Webdam permite que você aproveite seu próprio código interno e confie no Webdam como o sistema de arquivos que armazena e organiza suas imagens.</p> 
+						<p><a href="https://www.damsuccess.com/hc/en-us/articles/202134055-REST-API" class="btn btn-primary" role="button">Saiba Mais</a> 
+					</div> 
+				</div> 
+			</div> 
+			
+			<div class="col-sm-6 col-md-4"> 
+				<div class="thumbnail"> 
+					<img src="/betabase_alfa/app/img/drive.jpg" class="img-thumbnail"> 
+					<div class="caption"> 
+						<h3>Google Drive</h3> 
+						<p>Você pode, por exemplo, criar e compartilhar documentos do Google Docs com a extensa API REST do Google Drive. 
+						   O alvo principal são empresas que desejam uma maneira fácil de gerenciar relatórios automatizados regulares (e outros documentos) 
+						   em um local centralizado. A API do Google Drive permite criar aplicativos que aproveitam o armazenamento em nuvem do Google Drive. 
+						   Você pode desenvolver aplicativos que se integram ao Google Drive e criar funcionalidades robustas em seu aplicativo usando a API do Google Drive.</p> 
+						<p><a href="https://developers.google.com/drive" class="btn btn-primary" role="button">Saiba Mais</a>
+					</div> 
+				</div> 
+			</div> 
+			
+			<div class="col-sm-6 col-md-4"> 
+				<div class="thumbnail"> 
+					<img src="/betabase_alfa/app/img/googlear.png" class="img-thumbnail"> 
+					<div class="caption"> 
+						<h3>Google AR</h3> 
+						<p>Se uma imagem vale mais que mil palavras, quanto valeria se você adicionasse a elas pequenas bolhas de texto e detalhes que expliquem exatamente o que estamos vendo? 
+						   A Realidade Aumentada (AR) oferece a oportunidade de adicionar muitos desses detalhes animados em tempo real ao que quer que nossa câmera veja - e compartilhar esses objetos virtuais e diagramas com outras pessoas. 
+						   O Google quer construir o backbone que suporte essas interações de RA.</p> 
+						<p><a href="https://arvr.google.com/ar/" class="btn btn-primary" role="button">Saiba Mais</a>
+					</div> 
+				</div> 
+			</div>
+
+		</div>
+	</div>
+</div>
